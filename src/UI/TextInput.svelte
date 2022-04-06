7e3783052bcecfb9f52e966e62de2b663c7b8259
Radu Terec <raduterec@gmail.com>
2022-04-06 11:49:57 +0200

    Added touched to TextInput

--- src/UI/TextInput.svelte
+++ src/UI/TextInput.svelte
@@ -7,6 +7,12 @@
   export let type = "text";
   export let valid = true;
   export let validityMessage = "";
+
+  let touched = false;
+
+  function touch() {
+    touched = true;
+  }
 </script>
 
 <style>
@@ -55,11 +61,13 @@
 <div class="form-control">
   <label for={id}>{label}</label>
   {#if controlType === 'textarea'}
-    <textarea class:invalid="{!valid}" {rows} {id} {value} on:input />
+    <textarea class:invalid="{!valid && touched}" {rows} {id} {value} on:input
+      on:blur="{touch}" />
   {:else}
-    <input class:invalid="{!valid}" {type} {id} {value} on:input />
+    <input class:invalid="{!valid && touched}" {type} {id} {value} on:input 
+      on:blur="{touch}"/>
   {/if}
-  {#if !valid && validityMessage}
+  {#if !valid && validityMessage && touched}
     <p class="error-message">{validityMessage}</p>
   {/if}
 </div>
