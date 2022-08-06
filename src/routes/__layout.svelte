ee2990802bd3e8cf612081b912df4de50c1377be
Radu Terec <raduterec@betterask.erni>
2022-08-06 18:40:09 +0200

    Refactored MeetupGrid to svelteKit and TS. Data is fetched on the server. Simplified layout.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -8,18 +8,8 @@
 	<slot />
 </main>
 
-<footer>
-	<p>Regular Company name, Bunzli Str 12, 8001 Zurich</p>
-</footer>
-
 <style>
 	main {
-		margin-top: 4rem;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 40px 0;
-		}
+		margin-top: 5rem;
 	}
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,26 +1,88 @@
-<script context="module" lang="ts">
-	export const prerender = true;
+<script context="module">
+	export async function load({ fetch }) {
+		const url = `https://meetupsvelte-default-rtdb.europe-west1.firebasedatabase.app/meetup.json`;
+		const response = await fetch(url);
+		const data = await response.json();
+
+		const loadedMeetups = [];
+		for (const key in data) {
+			loadedMeetups.push({
+				...data[key],
+				id: key
+			});
+		}
+
+		return {
+			props: {
+				loadedMeetups
+			}
+		};
+	}
+</script>
+
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	import { fade } from 'svelte/transition';
+	import { flip } from 'svelte/animate';
+	import MeetupItem from '$lib/Meetup/MeetupItem.svelte';
+	import MeetupFilter from '$lib/Meetup/MeetupFilter.svelte';
+	import Button from '$lib/Ui/Button.svelte';
+
+	let favsOnly = false;
+	const dispatch = createEventDispatcher();
+
+	export let loadedMeetups: any[];
+
+	$: filteredMeetups = favsOnly ? loadedMeetups.filter((m) => m.isFavorite) : loadedMeetups;
+
+	function setFilter(event: { detail: string }) {
+		favsOnly = event.detail === 'favs';
+	}
+
+	function createMeetup() {
+		dispatch('add');
+	}
 </script>
 
 <svelte:head>
 	<title>Meet Us</title>
-	<meta name="description" content="Meet Us app" />
 </svelte:head>
 
-<section>
-	<h1>Welcome to your SvelteKit app</h1>
+<section id="meetup-controls">
+	<MeetupFilter on:select={setFilter} />
+	<Button on:click={createMeetup}>New Meetup</Button>
+</section>
+{#if filteredMeetups.length === 0}
+	<p id="no-meetups">No meetups found, you can start adding some.</p>
+{/if}
+<section id="meetups">
+	{#each filteredMeetups as meetup (meetup.id)}
+		<div transition:fade animate:flip={{ duration: 300 }}>
+			<MeetupItem item={meetup} on:detail on:edit />
+		</div>
+	{/each}
 </section>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
+	#meetups {
+		width: 100%;
+		display: grid;
+		grid-template-columns: 1fr 1fr;
 	}
 
-	h1 {
-		width: 100%;
+	@media screen and (min-width: 768px) {
+		#meetups {
+			grid-template-columns: repeat (2, 1fr);
+		}
+	}
+
+	#meetup-controls {
+		margin: 1rem;
+		display: flex;
+		justify-content: space-between;
+	}
+
+	#no-meetups {
+		margin: 1rem;
 	}
 </style>
